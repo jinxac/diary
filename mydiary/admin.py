9a49161c4a9a2f6d804a33632c8cdee476af4149
Harkirat Saluja <harkiratsaluja@Harkirats-MacBook-Air.local>
2021-07-02 22:38:36 +0530

    minor changes

--- mydiary/admin.py
+++ mydiary/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Content
 
 # Register your models here.
+class ContentAdmin(admin.ModelAdmin):
+    fields = ('title', 'text')
+    list_display = ('title',)
+
+admin.site.register(Content, ContentAdmin)
