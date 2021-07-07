a055ce7af4e210647c354c7ab79977119fd112bc
Harkirat Saluja <harkiratsaluja@Harkirats-MacBook-Air.local>
2021-07-07 16:45:28 +0530

    adding blog

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Blog(models.Model):
+  title=models.CharField(max_length=255)
+  content=models.TextField()
+  created_at=models.DateTimeField(default=timezone.now)
+  published_at=models.DateTimeField(null=True)
+  is_published=models.BooleanField(default=False)
