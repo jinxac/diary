f97b61f462c8113f52cd280ca242b5ee7d4704a2
Harkirat Saluja <harkiratsaluja@Harkirats-MacBook-Air.local>
2021-07-07 18:30:18 +0530

    blog summary field

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 # Create your models here.
 class Blog(models.Model):
   title=models.CharField(max_length=255)
+  summary=models.TextField(null=True)
   content=models.TextField()
   created_at=models.DateTimeField(default=timezone.now)
   published_at=models.DateTimeField(null=True)
