0026e2ef3503debd6e1f9e3680fd1f8ff5ec8c97
Harkirat Saluja <harkiratsaluja@Harkirats-MacBook-Air.local>
2021-07-07 17:17:27 +0530

    static files

--- diary/settings.py
+++ diary/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 # Application definition
 
